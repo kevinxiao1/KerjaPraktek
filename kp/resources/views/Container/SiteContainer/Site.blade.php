92e56522e308dcf9bf9fd5556a9efdcd819b90a8
hanselsantoso <hanselsantoso22@gmail.com>
2020-06-02 08:23:46 +0700

    title

--- kp/resources/views/Container/SiteContainer/Site.blade.php
+++ kp/resources/views/Container/SiteContainer/Site.blade.php
@@ -5,7 +5,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>aranoz</title>
+    <title>Sinar Surya Diesel</title>
     <link rel="icon" href="{{asset('Site/img/favicon.png')}}">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="{{asset('Site/css/bootstrap.min.css')}}">
