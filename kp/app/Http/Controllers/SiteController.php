c41592b745634886dea079cfbc0f4129ea99ed63
hanselsantoso <hanselsantoso22@gmail.com>
2020-06-02 09:23:43 +0700

    status barang

--- kp/app/Http/Controllers/SiteController.php
+++ kp/app/Http/Controllers/SiteController.php
@@ -23,7 +23,7 @@ class siteController extends Controller
     public function Products(Request $request)
     {
         // $daftarBarang = barang::all();
-        $daftarBarang = DB::table('barang')->paginate(9);
+        $daftarBarang = DB::table('barang')->where('status_barang','=',1)->paginate(9);
         $daftarKategori = kategori::all();
         $daftarSubKategori = subkategori::all();
         $kategoriTerpilih = "";
@@ -45,12 +45,14 @@ class siteController extends Controller
     {
         $daftarBarang = DB::table('barang')
         ->where('nama_barang', 'LIKE', "%{$request->search}%") 
+        ->where('status_barang','=',1)
         // ->orWhere('id_kategori', 'LIKE', "%{$request->search}%") 
         // ->orWhere('id_subkategori', 'LIKE', "%{$request->search}%") 
         // ->orWhere('deskripsi_barang', 'LIKE', "%{$request->search}%") 
         ->paginate(9);
         $jumlahBarang = DB::table('barang')
         ->where('nama_barang', 'LIKE', "%{$request->search}%") 
+        ->where('status_barang','=',1)
         // ->orWhere('id_kategori', 'LIKE', "%{$request->search}%") 
         // ->orWhere('id_subkategori', 'LIKE', "%{$request->search}%") 
         // ->orWhere('deskripsi_barang', 'LIKE', "%{$request->search}%") 
@@ -75,11 +77,11 @@ class siteController extends Controller
     }
     public function ProductsKategori(Request $request, $kategori)
     {
-        $daftarBarang = DB::table('barang')->where('id_kategori',$kategori)->paginate(9);
+        $daftarBarang = DB::table('barang')->where('id_kategori',$kategori)->where('status_barang','=',1)->paginate(9);
         $daftarKategori = kategori::all();
         $daftarSubKategori = subkategori::select()->where('id_kategori','=',$kategori)->get();
         // dd($daftarSubKategori);
-        $jumlahBarang = DB::table('barang')->where('id_kategori',$kategori)->count();
+        $jumlahBarang = DB::table('barang')->where('id_kategori',$kategori)->where('status_barang','=',1)->count();
         // $sub = kategori::find($kategori);
         $kategoriTerpilih = $kategori;
         $subKategoriTerpilih = $daftarSubKategori;
@@ -98,14 +100,14 @@ class siteController extends Controller
 
     public function ProductsSubKategori(Request $request, $subkategori)
     {
-        $daftarBarang = DB::table('barang')->where('id_subkategori',$subkategori)->paginate(9);
+        $daftarBarang = DB::table('barang')->where('id_subkategori',$subkategori)->where('status_barang','=',1)->paginate(9);
         $daftarKategori = kategori::all();
         $daftarSubKategori = subkategori::all();
         $kategori = subkategori::find($subkategori);
         // dd($kategori['id_kategori']);
         $kategoriTerpilih = $kategori['id_kategori'];
         $subKategoriTerpilih = $subkategori;
-        $jumlahBarang = DB::table('barang')->where('id_subkategori',$subkategori)->count();
+        $jumlahBarang = DB::table('barang')->where('id_subkategori',$subkategori)->where('status_barang','=',1)->count();
         $search = "";
         // $jumlahBarang = DB::table('barang')->count();
         return view('Content.SiteContent.Katalog.viewKatalog',
